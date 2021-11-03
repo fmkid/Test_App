9dd05c4202f365fb1f6656e38e509931e2e37db5
Ffmkid <fabian.morecam@gmail.com>
2021-11-02 23:26:29 -0500

    Added a button and a main text change functionality

--- app/src/main/java/com/fmkid/testapp/MainActivity.java
+++ app/src/main/java/com/fmkid/testapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.fmkid.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView lbl = findViewById(R.id.mainText);
+        Button btn = findViewById(R.id.boton);
+
+        btn.setOnClickListener(v -> cambiarTexto(lbl));
+    }
+
+    public void cambiarTexto(TextView label) {
+        if(label.getText().equals(getString(R.string.main_text)))
+            label.setText(R.string.sec_text);
+        else
+            label.setText(R.string.main_text);
     }
 }
